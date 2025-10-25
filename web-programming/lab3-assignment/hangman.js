78ce6543f25d8bf0db6145b1c158f3b51db63fc1
Barborica <mirea.cristian2004@gmail.com>
2025-10-25 21:41:08 +0300

    Added keyboard and word mechanics
    Added keyboard builder, game updater and guess handler, render word and initial setup

--- web-programming/lab3-assignment/hangman.js
+++ web-programming/lab3-assignment/hangman.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("hangmanCanvas");
+var canvas = document.getElementById("hangmanCanvas"); //cauta in index.html elementul cu id-ul "hangmanCanvas"
 var ctx = canvas.getContext("2d");
 
 ctx.lineWidth = 6;
@@ -28,4 +28,73 @@ var drawPillar = function() {
     ctx.stroke();
 }
 
-drawPillar();
+//valori initiale
+var secret = "MASINA";
+var answerVector=[];
+var lettersLeft = 0;
+
+var wordElement = document.getElementById("word"); //cauta in index.html elementul cu id-ul "word"
+var keyboardElement = document.getElementById("keyboard"); //cauta in index.html elementul cu id-ul "keyboard"
+
+//functie care initializeaza vectorul raspuns
+var setupAnswerVector = function(word) {
+    var v=[];
+    for (var i=0; i<word.length; i++) {
+        v.push("_");
+    }
+return v;
+}
+
+var renderWord = function(v) {
+    wordElement.textContent = v.join(" ");
+}
+
+//functie care construieste tastatura
+var buildKeyboard = function() {
+    keyboardElement.innerHTML = ""; //curata tastatura anterioara
+    var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+    for (var i=0; i<letters.length; i++) {
+        (function(ch){
+            var button = document.createElement("button");
+            button.className = "key";
+            button.type = "button";
+            button.textContent = ch;
+            button.onclick = function() {
+                handleGuess(ch);
+            }
+            keyboardElement.appendChild(button);
+        })(letters[i]);
+    }
+}
+
+//functie care actualizeaza jocul in urma unei incercari
+var updateGame = function(letters, word, vector){
+    var hits = 0;
+    for (var i=0; i<word.length; i++) {
+        if (word[i] === letters && vector[i] === "_") {
+            vector[i] = letters;
+            hits++;
+        }
+    }
+    return hits;
+}
+
+//functie care gestioneaza incercarile reusite
+var handleGuess = function(ch) {
+    ch = ch.toUpperCase();
+    var hits = updateGame(ch, secret, answerVector);
+    if (hits > 0) {
+        renderWord(answerVector);
+    }
+}
+
+//initializare joc
+var init = function() {
+    drawPillar();
+    answerVector = setupAnswerVector(secret);
+    lettersLeft = secret.length;
+    renderWord(answerVector);
+    buildKeyboard();
+}
+
+init();
